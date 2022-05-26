6b111c1c747f08f61f65bf4c8beaa569ecff3b95
farid <farid.raad@gmail.com>
2022-05-27 00:14:14 +0430

    test

--- src/main/java/org/farid/ShopApplication.java
+++ src/main/java/org/farid/ShopApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ShopApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
     }
